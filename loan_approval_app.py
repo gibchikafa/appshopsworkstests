b2a75d05207f8d52166b41fa960ffa813e10b5ea
Gibson Chikafa <gibbschikafa@gmail.com>
2026-06-04 14:45:59 +0200

    Loan approval app bolder text

--- loan_approval_app.py
+++ loan_approval_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
 st.set_page_config(page_title="Coming Soon", layout="centered")
-
-st.write("System under development. We are opening soon!!")
+st.markdown(
+    """
+    <div style="text-align: center; font-size: 3rem; font-weight: 700; line-height: 1.2;">
+        System under development. We are opening soon!!
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
